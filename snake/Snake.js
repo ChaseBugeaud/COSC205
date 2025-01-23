ed3ada167775b064fc57eb3595b6d2838d8579ef
Clara <168499053+ClaraEliot@users.noreply.github.com>
2025-01-23 10:30:36 -0800

    Started Snake Class

---
+++ snake/Snake.js
@@ -0,0 +1,17 @@
+class Snake {
+	headTile = {x: xCoord, y: yCoord};
+	segmentTiles = [{x: xCoord, y: yCoord}];
+	
+	constructor(){
+		
+	}
+	
+	grow(){
+		//Add location to the end of list
+	}
+	move(){
+		//Give headTile new location
+		//Iterate through list and shift forward by adding 
+		//headTile-location to front of list and disgarding last element
+	}
+}
