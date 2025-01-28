031beefa20b9748f31d30fbce6476bf2873b0db2
ChaseBugeaud <cbugeaud@shaw.ca>
2025-01-28 10:07:56 -0800

    copied Snake.js to main.js

--- snake/main.js
+++ snake/main.js
@@ -1,4 +1,50 @@
+class Snake {
+  snakeTiles = [{ x: xCoord, y: yCoord }];
 
+  movementStates = Object.freeze({
+    LEFT: 0,
+    RIGHT: 1,
+    UP: 2,
+    DOWN: 3
+  });
+
+  constructor(xCoord, yCoord) {
+    snakeTiles[0] = { x: xCoord, y: yCoord };
+  }
+
+  grow() {
+    //Add location to the end of list
+
+  }
+  move(direction, isGrowing) {
+    //TODO: check for apple, make apple the snake head
+    if (isGrowing) {
+      grow();
+    }
+
+    if (direction == this.movementStates.LEFT) {
+
+    } else if (direction == this.movementStates.RIGHT) {
+
+    } else if (direction == this.movementStates.UP) {
+
+    } else if (direction == this.movementStates.DOWN) {
+
+    }
+
+    function bark() {
+      //TODO: remove, for import testing
+      console.log("the snake barks");
+    }
+
+
+
+    //Give headTile new location
+    //Iterate through list and shift forward by adding 
+    //headTile-location to front of list and disgarding last element
+  }
+
+}
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -108,6 +154,7 @@ function randomCoord() {
 
 function drawApple(appleX, appleY) {
   console.log("aaple x ", appleX, "apple y", appleY)
+  console.log("apple count: ", apples.length)
   ctx.fillRect(appleX * TILE_SIZE, appleY * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
