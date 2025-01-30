760c2e8abc2827d87f7cbab9025fc4b6597f445b
Clara <168499053+ClaraEliot@users.noreply.github.com>
2025-01-30 13:40:14 -0800

    Fixed Start/End Screen

--- snake/endScreen.js
+++
@@ -1,9 +0,0 @@
-const startScreen = document.getElementById("startScreen");
-const playAgainButton = document.getElementById("playAgainButton");
-const endScreen = document.getElementById("endScreen");
-const gameCanvas = document.getElementById("canvas");
-
-playAgainButton.addEventListener("click", () => {
-	endScreen.style.display = "none";
-	startScreen.style.display = "block";
-});
